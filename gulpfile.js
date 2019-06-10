fd12147d69e2c11c0d478d5b4ddd422ff4120b60
WildPastry <mykdsn@gmail.com>
2019-06-10 18:32:44 +1200

    Added concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,16 @@
-import gulp from 'gulp';
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var less = require('gulp-less');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+var concat = require('gulp-concat');
+
+gulp.task('css', function() {
+	var processors = [autoprefixer, cssnano];
+	return gulp
+		.src('assets/less/**/*.less')
+		.pipe(concat('nonproflite'))
+		.pipe(less())
+		.pipe(postcss(processors))
+		.pipe(gulp.dest('assets/css'));
+});
