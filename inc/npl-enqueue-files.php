86b06317cef9c38cc90f2683b3c47fad23325491
WildPastry <mykdsn@gmail.com>
2019-06-17 20:19:38 +1200

    Adjustment to enqueue

--- inc/npl-enqueue-files.php
+++ inc/npl-enqueue-files.php
@@ -7,7 +7,7 @@
 function enqueue_files()
 {
 	// css
-	// fonts
+	// fonts ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 	// fira sans
 	wp_enqueue_style('nonproflite_fonts', get_template_directory_uri() . 'https://fonts.googleapis.com/css?family=Fira+Sans:300,400,700&display=swap', array(), '1.0');
 	//   merriweather
@@ -15,28 +15,34 @@ function enqueue_files()
 
 	//   quicksand
 	// 'https://fonts.googleapis.com/css?family=Quicksand:300,400,700&display=swap'
+	// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
-	// vendor
+	// vendor –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 	// bootstrap
 	wp_enqueue_style('nonproflite_bootstrap_css', get_template_directory_uri() . '/assets/css/vendor/bootstrap.min.css', array(), '4.1.3');
+	// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
-	// custom
+	// custom –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 	// nonproflite
 	wp_enqueue_style('nonproflite_css', get_template_directory_uri() . '/assets/css/nonproflite.css', array(), '1.0');
+	// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
 	// js
-	// vendor
+	// vendor –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 	// jquery
 	wp_enqueue_script('nonproflite_jquery_js', get_template_directory_uri() . '/assets/js/vendor/jquery.min.js', array(), '3.3.1', true);
 	// bootstrap
 	wp_enqueue_script('nonproflite_bootstrap_js', get_template_directory_uri() . '/assets/js/vendor/bootstrap.min.js', array(), '4.1.3', true);
 	// popper
 	wp_enqueue_script('nonproflite_popper_js', get_template_directory_uri() . '/assets/js/vendor/popper.min.js', array(), '1.0.0', true);
+	// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
-	// custom
+	// custom –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 	// nonproflite
 	wp_enqueue_script('nonproflite_js', get_template_directory_uri() . '/assets/js/nonproflite.js', array(), '1.0', true);
 	// nonproflite admin
 	wp_enqueue_script('nonproflite_admin_js', get_template_directory_uri() . '/assets/js/nonproflite-admin.js', array(), '1.0', true);
+	// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
 }
 add_action('wp_enqueue_scripts', 'enqueue_files');
