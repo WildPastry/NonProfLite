a4a197ceaa104e3f9682127641e359ee2a62328e
WildPastry <mykdsn@gmail.com>
2019-06-21 09:50:03 +1200

    Added WooCommerce

--- inc/functions/plugins/npl-plugins.php
+++ inc/functions/plugins/npl-plugins.php
@@ -4,7 +4,7 @@
  * @package nonproflite
  */
 
-// register the required plugins for this theme
+// Register the required plugins for this theme
 function nonproflite_register_required_plugins() {
 	/*
 	 * Array of plugin arrays. Required keys are name and slug.
@@ -18,14 +18,14 @@ function nonproflite_register_required_plugins() {
 		// 	'slug'               => 'tgm-example-plugin', // The plugin slug (typically the folder name).
 		// 	'source'             => get_template_directory() . '/lib/plugins/tgm-example-plugin.zip', // The plugin source.
 		// 	'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-		// 	'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+		// 	'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher.
 		// 	'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 		// 	'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
 		// 	'external_url'       => '', // If set, overrides default API URL and points to an external URL.
 		// 	'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
 		// ),
 
-		// This is an example of how to include a plugin from the WordPress Plugin Repository.
+		// Include WooCommerce from the WordPress Plugin Repository.
 		array(
 			'name'      => 'WooCommerce',
 			'slug'      => 'woocommerce',
