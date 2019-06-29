e23875b8997745009615956e463d7b97fc9d5d55
WildPastry <mykdsn@gmail.com>
2019-06-30 11:35:11 +1200

    Updating client instructions, updating media queries

--- assets/js/nonproflite.js
+++ assets/js/nonproflite.js
@@ -1,8 +1,1 @@
-console.log('ts connected...');
-// image hover
-$(document).on("mouseenter", ".tile", function () {
-    $(this).find(".tile-colour").toggle();
-});
-$(document).on("mouseleave", ".tile", function () {
-    $(this).find(".tile-colour").toggle();
-});
+console.log("ts connected..."),$(document).on("mouseenter",".tile",function(){$(this).find(".tile-colour").toggle()}),$(document).on("mouseleave",".tile",function(){$(this).find(".tile-colour").toggle()});
